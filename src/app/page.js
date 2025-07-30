75f45adbb8664347f7930c096f5e1e62766b014b
Thaarakenth C P <thaarakenthcp@gmail.com>
2025-07-30 10:33:52 +0530

    Repo Initialization

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,315 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import "./globals.css";
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [scanImage, setScanImage] = useState(null);
+  const [toggleScanDiagnosis, setToggleScanDiagnosis] = useState(false);
+  const [toggleBloodDiagnosis, setToggleBloodDiagnosis] = useState(false);
+  const [serverResponseStatus, setServerResponseStatus] = useState(false);
+  const [serverResponseData, setServerResponseData] = useState(null)
+  const [showScanSummary, setShowScanSummary] = useState(false);
+  const [showBloodSummary, setShowBloodSummary] = useState(false);
+  
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        setScanImage(URL.createObjectURL(file));
+    }
+  };
+  
+  const submitForm = async () => {
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setServerResponseData(result);
+        setServerResponseStatus(true);
+      } else {
+        alert('Form submission failed with status:', response.status);
+      }
+    } catch (error) {
+      alert('Error during form submission:', error);
+    }
+  };
+
+  return (
+    <div className="body">
+
+      <div className="header">
+        <p className="display-1">ThyroNetV6</p>
+        <p className="h4">AI-Powered Thyroid Diagnosis Platform</p>
+        <hr/>
+      </div>
+
+      <p className="h4">Patient Details</p>
+      
+      <div className="form">
+        <form 
+          id="myForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            submitForm();
+          }}
+        >
+        <div className="form-floating">
+          <input
+              type="text"
+              name="name"
+              id="name"
+              className="form-control shadow"
+              placeholder="Patient Name"
+              required></input>
+          <label htmlFor="name">Patient Name</label>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="form-floating">
+          <input
+              type="number"
+              name="age"
+              id="age"
+              className="form-control shadow"
+              placeholder="Age"
+              min={1}
+              max={120}
+              required></input>
+          <label htmlFor="age">Age</label>
+        </div>
+        <div className="form-floating">
+          <select
+            className="form-select shadow"
+            placeholder="Select Gender"
+            aria-label="Default select example"
+            id="gender"
+            name="gender"
+            required>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <label htmlFor="gender">Gender</label>
+        </div>
+
+        <div className="form-switch">
+          <input
+              type="checkbox"
+              name="toggleScanDiagnosis"
+              id="toggleScanDiagnosis"
+              className="form-check-input"
+              onClick={() => {
+                document.getElementById('toggleScanDiagnosis').checked
+                  ? setToggleScanDiagnosis(true)
+                  : setToggleScanDiagnosis(false);
+                setScanImage(null);
+              }}
+            ></input>
+          <label
+            htmlFor="toggleScanDiagnosis"
+            className="form-check-label"
+            >&nbsp; Ultrasound Scan
+          </label>
+
+          <br/>
+
+          <input
+              type="checkbox"              
+              name="toggleBloodDiagnosis"
+              id="toggleBloodDiagnosis"
+              className="form-check-input"
+              onClick={() => {
+                document.getElementById('toggleBloodDiagnosis').checked
+                  ? setToggleBloodDiagnosis(true)
+                  : setToggleBloodDiagnosis(false);
+              }}
+            ></input>
+          <label
+            htmlFor="toggleBloodDiagnosis"
+            className="form-check-label"
+            >&nbsp; Blood Test</label>
+          </div>
+
+          {toggleScanDiagnosis && (
+          <div>
+            <hr/>
+            <p className="h5">Ultrasound Scan Image</p>
+            <input
+              type="file"
+              name="scanImage"
+              id="scanImage"
+              className="form-control shadow"
+              accept="image/*"
+              onChange={handleImageChange}
+              required
+            ></input>
+            {/* <label htmlFor="scanImage">Ultrasound Scan Image</label> */}
+          </div>
+          )}
+
+          {scanImage && <img
+                          src={scanImage} 
+                          alt="Preview"
+                          ></img>
+          }
+
+          {toggleBloodDiagnosis && (
+          <div>
+          <hr/>
+          <p className="h5">Blood Test Values</p>
+          <div className="bloodTestInput row row-cols-2 g-3">
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="tsh"
+                id="tsh"
+                className="form-control form-control shadow"
+                placeholder="TSH"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="tsh">TSH</label>
+            </div>
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="freeT4"
+                id="freeT4"
+                className="form-control form-control shadow"
+                placeholder="Free T4"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="freeT4">Free T4</label>
+            </div>
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="freeT3"
+                id="freeT3"
+                className="form-control form-control shadow"
+                placeholder="Free T3"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="freeT3">Free T3</label>
+            </div>
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="totalT4"
+                id="totalT4"
+                className="form-control form-control shadow"
+                placeholder="Total T4"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="totalT4">Total T4</label>
+            </div>
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="antiTpo"
+                id="antiTpo"
+                className="form-control form-control shadow"
+                placeholder="AntiTPO"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="antiTpo">AntiTPO</label>
+            </div>
+            <div className="form-floating col">
+              <input
+                type="number"
+                name="antiTg"
+                id="antiTg"
+                className="form-control form-control shadow"
+                placeholder="AntiTg"
+                min={0}
+                required
+                ></input>
+              <label htmlFor="antiTg">AntiTg</label>
+            </div>
+          </div> 
+        </div>
+        )}
+          <button
+            type="submit"
+            className="btn btn-primary submitButton"
+            >Submit</button>
+        </form>
+      </div>
+
+      {serverResponseStatus && 
+      <div className="testResults">
+        <hr/>
+        <p className="h5">Test Results</p>
+
+        {serverResponseData.data.scan_result.target ? 
+        <div
+          className="testResultWindow shadow"
+          onClick={()=>{
+            showScanSummary ? setShowScanSummary(false) : setShowScanSummary(true);
+          }}
+          >
+        <p className="h6">Ultrasound Diagnosis</p>
+        <p>Predicted Class : <strong>{serverResponseData.data.scan_result.target || ""}</strong></p>
+
+        {showScanSummary &&
+          <ul>
+            Confidence Levels :
+            <li>TIRADS 1 (Normal) : {serverResponseData.data.scan_result.target_summary["TIRADS1"]}%</li>
+            <li>TIRADS 2 (Benign) : {serverResponseData.data.scan_result.target_summary["TIRADS2"]}%</li>
+            <li>TIRADS 3 (Probably Benign) : {serverResponseData.data.scan_result.target_summary["TIRADS3"]}%</li>
+            <li>TIRADS 4 (Suspicious) : {serverResponseData.data.scan_result.target_summary["TIRADS4"]}%</li>
+            <li>TIRADS 5 (Highly Suspicious) : {serverResponseData.data.scan_result.target_summary["TIRADS5"]}%</li>
+          </ul>
+        }
+        </div> : ""
+        }
+        
+        {serverResponseData.data.blood_result.target ?
+        <div
+          className="testResultWindow shadow"
+          onClick={()=>{
+            showBloodSummary ? setShowBloodSummary(false) : setShowBloodSummary(true);
+        }}>
+        <p className="h6">Blood Test Diagnosis</p>
+        <p>Blood Results : <strong>{serverResponseData.data.blood_result.target || ""}</strong></p>
+
+        {showBloodSummary && 
+          <ul>
+            Confidence Levels :
+            <li>Normal                         : {serverResponseData.data.blood_result.target_summary["Normal"]}%                          </li>
+            <li>Central Hypothyroidism         : {serverResponseData.data.blood_result.target_summary["Central Hypothyroidism"]}%          </li>
+            <li>Euthyroid Sick Syndrome        : {serverResponseData.data.blood_result.target_summary["Euthyroid Sick Syndrome"]}%         </li>
+            <li>Graves' Disease                : {serverResponseData.data.blood_result.target_summary["Graves' Disease"]}%                 </li>
+            <li>Hashimoto's Thyroiditis        : {serverResponseData.data.blood_result.target_summary["Hashimoto's Thyroiditis"]}%         </li>
+            <li>Hyperthyroidism                : {serverResponseData.data.blood_result.target_summary["Hyperthyroidism"]}%                 </li>
+            <li>Hypothyroidism                 : {serverResponseData.data.blood_result.target_summary["Hypothyroidism"]}%                  </li>
+            <li>Iodine-Induced Hyperthyroidism : {serverResponseData.data.blood_result.target_summary["Iodine-Induced Hyperthyroidism"]}%  </li>
+            <li>Iodine-Induced Hypothyroidism  : {serverResponseData.data.blood_result.target_summary["Iodine-Induced Hypothyroidism"]}%   </li>
+            <li>Postpartum Thyroiditis         : {serverResponseData.data.blood_result.target_summary["Postpartum Thyroiditis"]}%          </li>
+            <li>Subclinical Hyperthyroidism    : {serverResponseData.data.blood_result.target_summary["Subclinical Hyperthyroidism"]}%     </li>
+            <li>Subclinical Hypothyroidism     : {serverResponseData.data.blood_result.target_summary["Subclinical Hypothyroidism"]}%      </li>
+            <li>Thyroid Hormone Resistance     : {serverResponseData.data.blood_result.target_summary["Thyroid Hormone Resistance"]}%      </li>
+          </ul>
+        }
+        
+        </div> : ""
+        }
+      </div>
+      }
+      
+      <div className="footer">
+        <hr/>
+        <p>Not really a frontend guy, so don't mind the UI</p>
+      </div>
     </div>
   );
 }
