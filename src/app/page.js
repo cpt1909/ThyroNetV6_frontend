3aec80749dcdd2af25983555146099802d018b9b
Thaarakenth C P <thaarakenthcp@gmail.com>
2025-07-30 19:33:38 +0530

    changes in env variables

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000', {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL, {
         method: 'POST',
         body: formData,
       });
